0065622374e91b30a052e25720decfebdd1bf454
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:28:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddUserPage from "./pages/AddUserPage";
+import EditUserPage from "./pages/EditUserPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add-user" element={<AddUserPage />} />
+        <Route path="/edit-user/:id" element={<EditUserPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
