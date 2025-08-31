749058c37c0d2348bb7db28e02758dd76efcdb81
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:30:10 +0530

    Update AddUserPage.jsx

--- src/pages/AddUserPage.jsx
+++ src/pages/AddUserPage.jsx
@@ -1,1 +1,23 @@
+import { createUser } from "../api/userService";
+import UserForm from "../components/users/UserForm";
+import { useNavigate } from "react-router-dom";
 
+const AddUserPage = () => {
+  const navigate = useNavigate();
+
+  const handleAddUser = (user) => {
+    createUser(user).then((res) => {
+      alert("User added successfully!");
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">Add New User</h2>
+      <UserForm onSubmit={handleAddUser} />
+    </div>
+  );
+};
+
+export default AddUserPage;
