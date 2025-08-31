c23a9df5915db935b050db08282aeed4b41d6028
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:29:17 +0530

    Update useUsers.js

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,1 +1,18 @@
+import { useState, useEffect } from "react";
+import { getUsers } from "../api/userService";
 
+const useUsers = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUsers().then((res) => {
+      setUsers(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  return { users, setUsers, loading };
+};
+
+export default useUsers;
