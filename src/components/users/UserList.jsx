15cdb3dfd88dd3d7fc532fdf24a4c0f65a7b850f
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:31:54 +0530

    Update UserList.jsx

--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -1,1 +1,11 @@
+import UserItem from "./UserItem";
 
+const UserList = ({ users, onDelete }) => (
+  <div className="grid gap-4">
+    {users.map((user) => (
+      <UserItem key={user.id} user={user} onDelete={onDelete} />
+    ))}
+  </div>
+);
+
+export default UserList;
