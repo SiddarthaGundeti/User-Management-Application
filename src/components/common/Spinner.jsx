7f12f49fcd516e3bb849b82a23c3d0d139b071db
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:32:42 +0530

    Update Spinner.jsx

--- src/components/common/Spinner.jsx
+++ src/components/common/Spinner.jsx
@@ -1,1 +1,8 @@
+const Spinner = () => (
+  <div className="flex justify-center items-center p-4">
+    <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+  </div>
+);
+
+export default Spinner;
 
