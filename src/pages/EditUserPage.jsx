6c6a312f4d59bb29d59aec37dd042abf697a5f43
Siddartha Gundeti <174889426+SiddarthaGundeti@users.noreply.github.com>
2025-08-31 08:29:52 +0530

    Update EditUserPage.jsx

--- src/pages/EditUserPage.jsx
+++ src/pages/EditUserPage.jsx
@@ -1,1 +1,30 @@
+import { useEffect, useState } from "react";
+import { getUserById, updateUser } from "../api/userService";
+import UserForm from "../components/users/UserForm";
+import { useParams, useNavigate } from "react-router-dom";
 
+const EditUserPage = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUserById(id).then((res) => setUser(res.data));
+  }, [id]);
+
+  const handleUpdate = (updatedUser) => {
+    updateUser(id, updatedUser).then(() => {
+      alert("User updated successfully!");
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">Edit User</h2>
+      {user && <UserForm onSubmit={handleUpdate} initialData={user} />}
+    </div>
+  );
+};
+
+export default EditUserPage;
